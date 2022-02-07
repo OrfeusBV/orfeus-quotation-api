c9ef6e44079770ca29cfc93ebe18251c4822d7b8
Rens Jaspers <rens.jaspers@gmail.com>
2022-02-07 16:40:03 +0100

    Add shutdown hook

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  logger = new Logger(this.constructor.name);
   getHello(): string {
     return 'Hello World!';
+  }
+
+  onApplicationShutdown(signal: string) {
+    this.logger.log(`${signal}. Shutting down...`);
   }
 }
