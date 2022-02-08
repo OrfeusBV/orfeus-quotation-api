441e995817422576ad05bb56cf4a9deda59331a8
Rens Jaspers <rens.jaspers@gmail.com>
2022-02-08 16:27:23 +0100

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
   app.useGlobalInterceptors(new TimingInterceptor());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableShutdownHooks();
